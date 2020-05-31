6704a61d2265a83040b38fdad261c65ef8120a49
Duduling <duduling20@gmail.com>
2020-05-31 20:11:09 +0900

    feat: Add feature to changing color

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const canvas = document.getElementById('jsCanvas')
+const colors = document.getElementsByClassName('jsColor')
+const ctx = canvas.getContext('2d')
+
+ctx.strokeStyle = '#2c2c2c'
+ctx.lineWidth = 2.5
 
 let painting = false
 
@@ -8,9 +13,17 @@ const stopPainting = () => {
 
 const onMouseMove = e => {
     const { offsetX: x, offsetY: y } = e
+
+    if (!painting) {
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+    } else {
+        ctx.lineTo(x, y)
+        ctx.stroke()
+    }
 }
 
-const onMouseDown = () => {
+const startPainting = () => {
     painting = true
 }
 
@@ -18,11 +31,19 @@ const onMouseUp = () => {
     stopPainting()
 }
 
+const handleColorClick = e => {
+    const { target: { style: { backgroundColor: color } } } = e
+
+    ctx.strokeStyle = color
+}
+
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove)
-    canvas.addEventListener('mouseup', onMouseUp)
-    canvas.addEventListener('mousedown', onMouseDown)
+    canvas.addEventListener('mousedown', startPainting)
+    canvas.addEventListener('mouseup', stopPainting)
     canvas.addEventListener('mouseleave', stopPainting)
 }
 
-
+Array.from(colors).forEach(color => {
+    color.addEventListener('click', handleColorClick)
+})
