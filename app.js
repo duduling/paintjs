ab6173c4bc2e92d06fa0122b817e8445c4412842
Duduling <duduling20@gmail.com>
2020-05-31 20:29:31 +0900

    feat: Add feature to filling canvas

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const canvas = document.getElementById('jsCanvas')
 const colors = document.getElementsByClassName('jsColor')
+const range = document.getElementById('jsRange')
+const mode = document.getElementById('jsMode')
 const ctx = canvas.getContext('2d')
 
-ctx.strokeStyle = '#2c2c2c'
+const INITIAL_COLOR = '#2c2c2c'
+const CANVAS_SIZE = 700
+
+ctx.strokeStyle = INITIAL_COLOR
+canvas.width = CANVAS_SIZE
+canvas.height = CANVAS_SIZE
 ctx.lineWidth = 2.5
 
 let painting = false
+let filling = false
 
 const stopPainting = () => {
     painting = false
@@ -35,6 +43,24 @@ const handleColorClick = e => {
     const { target: { style: { backgroundColor: color } } } = e
 
     ctx.strokeStyle = color
+    ctx.fillStyle = color
+}
+
+const handleRangeChange = e => {
+    const { target: { value: size } } = e
+
+    ctx.lineWidth = size
+}
+
+const handleCanvasClick = () => {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+    }
+}
+
+const handleModeClick = () => {
+    filling = !filling
+    mode.innerText = filling ? 'Paint' : 'Fill'
 }
 
 if (canvas) {
@@ -42,7 +68,18 @@ if (canvas) {
     canvas.addEventListener('mousedown', startPainting)
     canvas.addEventListener('mouseup', stopPainting)
     canvas.addEventListener('mouseleave', stopPainting)
+    canvas.addEventListener('click', handleCanvasClick)
 }
+
+
+if (range) {
+    range.addEventListener('input', handleRangeChange)
+}
+
+if (mode) {
+    mode.addEventListener('click', handleModeClick)
+}
+
 
 Array.from(colors).forEach(color => {
     color.addEventListener('click', handleColorClick)
